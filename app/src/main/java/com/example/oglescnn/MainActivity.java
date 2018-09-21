1e159c389ce564e3482ad3dcc6d8055669d64bbc
koibiki <orient2008.cl@hotmail.com>
2018-09-21 09:49:51 +0800

    测试 squeeze net 1.1

--- app/src/main/java/com/example/oglescnn/MainActivity.java
+++ app/src/main/java/com/example/oglescnn/MainActivity.java
@@ -5,12 +5,12 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.example.eglnn.NnNetwork;
+import com.example.eglnn.layer.Concat;
 import com.example.eglnn.layer.ConvGEMM2;
 import com.example.eglnn.layer.Layer.PaddingType;
 import com.example.eglnn.layer.Input;
 import com.example.eglnn.layer.Layer;
 import com.example.eglnn.layer.Pooling;
-import com.example.eglnn.utils.DataUtils;
 import com.example.eglnn.utils.TestDataCreator;
 import com.example.eglnn.utils.Utils;
 
@@ -18,37 +18,161 @@ public class MainActivity extends AppCompatActivity {
 
     private NnNetwork mNnNetwork;
 
+    int width = 227;
+    int height = width;
+    int channel = 4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buildNet();
+        buildSqueezeNet();
 
+//        buildTestNet();
     }
 
-    int width = 112;
-    int height = width;
-    int channel = 8;
-
-    private void buildNet() {
+    private void buildTestNet() {
         mNnNetwork = new NnNetwork(this);
 
         Layer in = new Input(this, width, height, channel);
         mNnNetwork.addLayer(in);
 
-        Layer conv1 = new ConvGEMM2(this, in, 512, 1, 1, PaddingType.SAME, 1, 1, Layer.ActiveType.RELU, "");
+
+        Layer conv1 = new ConvGEMM2(this, in, 64, 3, 3, PaddingType.VALID, 2, 2, Layer.ActiveType.RELU, "");
         mNnNetwork.addLayer(conv1);
 
-//        Pooling pooling = new Pooling(this, in, 3, 3, PaddingType.SAME, 2, 2);
-//        mNnNetwork.addLayer(pooling);
+        mNnNetwork.initialize();
+    }
+
+    private void buildSqueezeNet() {
+        mNnNetwork = new NnNetwork(this);
+
+        Layer in = new Input(this, width, height, channel);
+        mNnNetwork.addLayer(in);
+
+
+        Layer conv1 = new ConvGEMM2(this, in, 64, 3, 3, PaddingType.VALID, 2, 2, Layer.ActiveType.RELU, "");
+        mNnNetwork.addLayer(conv1);
+
+        Pooling pooling1 = new Pooling(this, conv1, 3, 3, PaddingType.VALID, 2, 2);
+        mNnNetwork.addLayer(pooling1);
+
+        // fire2
+        Layer conv2_squeeze = new ConvGEMM2(this, pooling1, 16, 1, 1, PaddingType.SAME, 1, 1, Layer.ActiveType.RELU, "");
+        mNnNetwork.addLayer(conv2_squeeze);
+
+        Layer conv2_1 = new ConvGEMM2(this, conv2_squeeze, 64, 1, 1, PaddingType.SAME, 1, 1, Layer.ActiveType.RELU, "");
+        mNnNetwork.addLayer(conv2_1);
+
+        Layer conv2_2 = new ConvGEMM2(this, conv2_squeeze, 64, 3, 3, PaddingType.SAME, 1, 1, Layer.ActiveType.RELU, "");
+        mNnNetwork.addLayer(conv2_2);
+
+        Concat concat2 = new Concat(this, new Layer[]{conv2_1, conv2_2}, 2);
+        mNnNetwork.addLayer(concat2);
+
+        // fire3
+        Layer conv3_squeeze = new ConvGEMM2(this, concat2, 16, 1, 1, PaddingType.SAME, 1, 1, Layer.ActiveType.RELU, "");
+        mNnNetwork.addLayer(conv3_squeeze);
+
+        Layer conv3_1 = new ConvGEMM2(this, conv3_squeeze, 64, 1, 1, PaddingType.SAME, 1, 1, Layer.ActiveType.RELU, "");
+        mNnNetwork.addLayer(conv3_1);
+
+        Layer conv3_2 = new ConvGEMM2(this, conv3_squeeze, 64, 3, 3, PaddingType.SAME, 1, 1, Layer.ActiveType.RELU, "");
+        mNnNetwork.addLayer(conv3_2);
+
+        Concat concat3 = new Concat(this, new Layer[]{conv3_1, conv3_2}, 2);
+        mNnNetwork.addLayer(concat3);
+
+        Pooling pooling3 = new Pooling(this, concat3, 3, 3, PaddingType.VALID, 2, 2);
+        mNnNetwork.addLayer(pooling3);
+
+        // fire4
+        Layer conv4_squeeze = new ConvGEMM2(this, pooling3, 32, 1, 1, PaddingType.SAME, 1, 1, Layer.ActiveType.RELU, "");
+        mNnNetwork.addLayer(conv4_squeeze);
+
+        Layer conv4_1 = new ConvGEMM2(this, conv4_squeeze, 128, 1, 1, PaddingType.SAME, 1, 1, Layer.ActiveType.RELU, "");
+        mNnNetwork.addLayer(conv4_1);
+
+        Layer conv4_2 = new ConvGEMM2(this, conv4_squeeze, 128, 3, 3, PaddingType.SAME, 1, 1, Layer.ActiveType.RELU, "");
+        mNnNetwork.addLayer(conv4_2);
+
+        Concat concat4 = new Concat(this, new Layer[]{conv4_1, conv4_2}, 2);
+        mNnNetwork.addLayer(concat4);
+
+        // fire5
+        Layer conv5_squeeze = new ConvGEMM2(this, concat4, 32, 1, 1, PaddingType.SAME, 1, 1, Layer.ActiveType.RELU, "");
+        mNnNetwork.addLayer(conv5_squeeze);
+
+        Layer conv5_1 = new ConvGEMM2(this, conv5_squeeze, 128, 1, 1, PaddingType.SAME, 1, 1, Layer.ActiveType.RELU, "");
+        mNnNetwork.addLayer(conv5_1);
+
+        Layer conv5_2 = new ConvGEMM2(this, conv5_squeeze, 128, 3, 3, PaddingType.SAME, 1, 1, Layer.ActiveType.RELU, "");
+        mNnNetwork.addLayer(conv5_2);
+
+        Concat concat5 = new Concat(this, new Layer[]{conv5_1, conv5_2}, 2);
+        mNnNetwork.addLayer(concat5);
+
+        Pooling pooling5 = new Pooling(this, concat5, 3, 3, PaddingType.VALID, 2, 2);
+        mNnNetwork.addLayer(pooling5);
+
+        // fire6
+        Layer conv6_squeeze = new ConvGEMM2(this, pooling5, 48, 1, 1, PaddingType.SAME, 1, 1, Layer.ActiveType.RELU, "");
+        mNnNetwork.addLayer(conv6_squeeze);
+
+        Layer conv6_1 = new ConvGEMM2(this, conv6_squeeze, 192, 1, 1, PaddingType.SAME, 1, 1, Layer.ActiveType.RELU, "");
+        mNnNetwork.addLayer(conv6_1);
+
+        Layer conv6_2 = new ConvGEMM2(this, conv6_squeeze, 192, 3, 3, PaddingType.SAME, 1, 1, Layer.ActiveType.RELU, "");
+        mNnNetwork.addLayer(conv6_2);
+
+        Concat concat6 = new Concat(this, new Layer[]{conv6_1, conv6_2}, 2);
+        mNnNetwork.addLayer(concat6);
+
+        // fire7
+        Layer conv7_squeeze = new ConvGEMM2(this, concat6, 48, 1, 1, PaddingType.SAME, 1, 1, Layer.ActiveType.RELU, "");
+        mNnNetwork.addLayer(conv7_squeeze);
+
+        Layer conv7_1 = new ConvGEMM2(this, conv7_squeeze, 192, 1, 1, PaddingType.SAME, 1, 1, Layer.ActiveType.RELU, "");
+        mNnNetwork.addLayer(conv7_1);
+
+        Layer conv7_2 = new ConvGEMM2(this, conv7_squeeze, 192, 3, 3, PaddingType.SAME, 1, 1, Layer.ActiveType.RELU, "");
+        mNnNetwork.addLayer(conv7_2);
+
+        Concat concat7 = new Concat(this, new Layer[]{conv7_1, conv7_2}, 2);
+        mNnNetwork.addLayer(concat7);
+
+        // fire8
+        Layer conv8_squeeze = new ConvGEMM2(this, concat7, 64, 1, 1, PaddingType.SAME, 1, 1, Layer.ActiveType.RELU, "");
+        mNnNetwork.addLayer(conv8_squeeze);
+
+        Layer conv8_1 = new ConvGEMM2(this, conv8_squeeze, 256, 1, 1, PaddingType.SAME, 1, 1, Layer.ActiveType.RELU, "");
+        mNnNetwork.addLayer(conv8_1);
+
+        Layer conv8_2 = new ConvGEMM2(this, conv8_squeeze, 256, 3, 3, PaddingType.SAME, 1, 1, Layer.ActiveType.RELU, "");
+        mNnNetwork.addLayer(conv8_2);
+
+        Concat concat8 = new Concat(this, new Layer[]{conv8_1, conv8_2}, 2);
+        mNnNetwork.addLayer(concat8);
+
+        // fire9
+        Layer conv9_squeeze = new ConvGEMM2(this, concat8, 64, 1, 1, PaddingType.SAME, 1, 1, Layer.ActiveType.RELU, "");
+        mNnNetwork.addLayer(conv9_squeeze);
+
+        Layer conv9_1 = new ConvGEMM2(this, conv9_squeeze, 256, 1, 1, PaddingType.SAME, 1, 1, Layer.ActiveType.RELU, "");
+        mNnNetwork.addLayer(conv9_1);
+
+        Layer conv9_2 = new ConvGEMM2(this, conv9_squeeze, 256, 3, 3, PaddingType.SAME, 1, 1, Layer.ActiveType.RELU, "");
+        mNnNetwork.addLayer(conv9_2);
+
+        Concat concat9 = new Concat(this, new Layer[]{conv9_1, conv9_2}, 2);
+        mNnNetwork.addLayer(concat9);
 
         mNnNetwork.initialize();
-
     }
 
     public void runNn(View view) {
-        float[][][] input = TestDataCreator.createInputBuffer(new int[]{width, height, channel});
+        float[][][] input = TestDataCreator.createInputBuffer(new int[]{width, height, channel}, 0);
         float[][] localInput = new float[Utils.alignBy4(channel) / 4][width * height * 4];
         for (int w = 0; w < width; w++) {
             for (int h = 0; h < height; h++) {
